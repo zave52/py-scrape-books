0b66446c44ec2e26226b2d5ba77d228fdf8c24f3
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-30 11:55:41 +0300

    create BookItem class

--- library/spiders/books.py
+++ library/spiders/books.py
@@ -1,6 +1,16 @@
 import scrapy
 
 
+class BookItem(scrapy.Item):
+    title: scrapy.Field()
+    price: scrapy.Field()
+    amount_in_stock: scrapy.Field()
+    rating: scrapy.Field()
+    category: scrapy.Field()
+    description: scrapy.Field()
+    upc: scrapy.Field()
+
+
 class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["books.toscrape.com"]
