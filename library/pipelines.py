ceb14fa00401be1daa58001da268dc58c991eb85
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-30 12:36:58 +0300

    setup LibraryPipeline

--- library/pipelines.py
+++ library/pipelines.py
@@ -2,12 +2,28 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import scrapy
+from scrapy.exporters import JsonItemExporter, JsonLinesItemExporter
 
 
 class LibraryPipeline:
-    def process_item(self, item, spider):
+    def __init__(self) -> None:
+        self.file = None
+        self.exporter = None
+
+    def open_spider(self, spider: scrapy.Spider) -> None:
+        self.file = open("books.jl", "wb")
+        self.exporter = JsonLinesItemExporter(self.file, encoding="utf-8")
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider: scrapy.Spider) -> None:
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(
+        self,
+        item: scrapy.Item,
+        spider: scrapy.Spider
+    ) -> scrapy.Item:
+        self.exporter.export_item(item)
         return item
