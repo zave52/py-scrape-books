d6ba4e7921056e26e6611c8463558397996614e3
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-30 12:00:02 +0300

    create _parse_single_book method for BookSpider

--- library/spiders/books.py
+++ library/spiders/books.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.http import Response
 
 
 class BookItem(scrapy.Item):
@@ -15,6 +16,43 @@ class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
+
+    RATING_MAP = {
+        "One": 1,
+        "Two": 2,
+        "Three": 3,
+        "Four": 4,
+        "Five": 5
+    }
+
+    def _parse_single_book(self, response: Response) -> BookItem:
+        book = BookItem()
+
+        book["title"] = response.css("div.product_main > h1::text").get()
+        book["price"] = float(
+            response.css(
+                ".price_color::text"
+            ).get().replace("£", "")
+        )
+        book["amount_in_stock"] = int(
+            response.css(
+                "p.instock.availability::text"
+            ).getall()[1].split()[2].replace("(", "")
+        )
+        book["rating"] = self.RATING_MAP[
+            response.css(
+                "p.star-rating::attr(class)"
+            ).get().split()[1]
+        ]
+        book["category"] = response.css(
+            "ul.breadcrumb > li > a::text"
+        ).getall()[2]
+        book["description"] = response.css(
+            "#product_description ~ p::text"
+        ).get()
+        book["upc"] = response.css(".table.table-striped td::text").get()
+
+        return book
 
     def parse(self, response):
         pass
